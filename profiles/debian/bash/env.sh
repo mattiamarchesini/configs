09785f4cbbcfa28a8737678d873a0273becc3816
Mattia Marchesini <mattia.marchesini1999@gmail.com>
2025-03-27 02:12:08 +0100

    first commit

---
+++ profiles/debian/bash/env.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -o allexport
+
+XDG_CONFIG_HOME=~/.config
+DEBIAN_FRONTEND=noninteractive
+
+set +o allexport
---
+++ profiles/unix/bash/env.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -o allexport
+
+XDG_CONFIG_HOME=~/.config
+
+set +o allexport
