e5314fd773da82fa972abc8401fd557fb7eca406
Mattia Marchesini <mattia.marchesini1999@gmail.com>
2025-07-09 20:27:36 +0200

    modify unix and windows install

--- windows/wsl/deploy.ps1
+++ windows/wsl/deploy.ps1
@@ -1,10 +1,13 @@
 # admin
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+
 # for enterprise edition
-dism.exe /online /enable-feature /featurename:Microsoft-Hyper-V-All /all /norestart
+$edition = (Get-CimInstance Win32_OperatingSystem).Caption
+if ($edition -match "Enterprise") {
+  dism.exe /online /enable-feature /featurename:Microsoft-Hyper-V-All /all /norestart
+}
 
 #reboot
-
-wsl --install -d fedora  --no-launch
-wsl --set-default fedora
+wsl --install --no-launch -d fedora
+wsl --set-default FedoraLinux-42
