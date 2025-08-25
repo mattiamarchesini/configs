826ce92c6f08aa91a2d73f9c518225b526b0e4bc
Mattia Marchesini <mattia.marchesini1999@gmail.com>
2025-08-25 19:12:50 +0200

    wsl deploy: add win pro edition, continue refactor script

--- windows/wsl/deploy.ps1
+++ windows/wsl/deploy.ps1
@@ -1,13 +1,16 @@
-# admin
-dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
-dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+# as admin
 
-# for enterprise edition
+# for Pro/Enterprise editions, use full Hyper-V stack (Hyper-V Manager, hypervisor, VM services)
 $edition = (Get-CimInstance Win32_OperatingSystem).Caption
-if ($edition -match "Enterprise") {
+if ($edition -match "Pro|Enterprise") {
   dism.exe /online /enable-feature /featurename:Microsoft-Hyper-V-All /all /norestart
 }
 
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+
 #reboot
+
+wsl --set-default-version 2
 wsl --install --no-launch -d fedora
 wsl --set-default FedoraLinux-42
